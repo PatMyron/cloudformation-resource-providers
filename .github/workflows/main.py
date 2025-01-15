e4b81445aa77656f7553510b2d46aa4b70e1a6db
Pat Myron <PatMyron@users.noreply.github.com>
2025-01-15 16:35:13 -0500

    include more repos
    https://github.com/PatMyron/cloudformation-resource-providers/issues/2
    
    also handles https://github.com/aws-cloudformation/aws-cloudformation-resource-provider-qbusiness

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -1,6 +1,9 @@
 import os
 import re
 import requests
-for repo in requests.get('https://api.github.com/orgs/aws-cloudformation/repos?type=public&per_page=100&sort=full_name').json():
-    if 'aws-cloudformation-resource-providers' in repo['html_url']:
-        os.system('git submodule add ' + repo['html_url'] + ' ' + re.sub('.*resource-providers-', '', repo['html_url']))
+url = 'https://api.github.com/orgs/aws-cloudformation/repos?type=public&per_page=100&sort=full_name'
+while url:
+  for repo in requests.get(url).json():
+      if 'aws-cloudformation-resource-provider' in repo['html_url']:
+          os.system('git submodule add ' + repo['html_url'] + ' ' + re.sub('.*resource-providers?-', '', repo['html_url']))
+  url = requests.get(url).links.get('next', {}).get('url')
